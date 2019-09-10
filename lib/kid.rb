bbd11ae5d75d016aab1173e07e76f4fccc5c75cc
Christian Perez <perezchristian1012@gmail.com>
2019-09-10 18:25:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,9 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
   include Dance
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
